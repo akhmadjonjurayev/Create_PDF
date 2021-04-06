840f7b70bf64566f39c1005b98ce5035b7cedde1
akhmadjonjurayev <formyproject1999@gmail.com>
2021-04-06 13:08:56 +0500

    files

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,71 @@
+﻿using DinkToPdf;
+using DinkToPdf.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Create_PDF.Model;
+using System.IO;
+
+namespace Create_PDF.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+        private readonly IConverter _converter;
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger,IConverter converter)
+        {
+            _converter = converter;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        [HttpGet("pdf")]
+        public IActionResult CreatePDF()
+        {
+            var globalSettings = new GlobalSettings
+            {
+                ColorMode = ColorMode.Color,
+                Orientation = Orientation.Portrait,
+                PaperSize = PaperKind.A4,
+                Margins = new MarginSettings { Top = 10 },
+                DocumentTitle = "PDF Report"
+            };
+            var objectSettings = new ObjectSettings
+            {
+                PagesCount = true,
+                HtmlContent = GetValue.GetHTML(),
+                WebSettings = { DefaultEncoding = "utf-8" },
+                HeaderSettings = { FontName = "Arial", FontSize = 9, Right = "Page [page] of [toPage]", Line = true },
+                FooterSettings = { FontName = "Arial", FontSize = 9, Line = true, Center = "Report Footer" }
+            };
+            var pdf = new HtmlToPdfDocument()
+            {
+                GlobalSettings = globalSettings,
+                Objects = { objectSettings }
+            };
+            var file = _converter.Convert(pdf);
+            return File(file, "application/pdf","A.pdf");
+        }
+    }
+}
---
+++ Model/Content.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Create_PDF.Model
+{
+    public static class GetValue
+    {
+        public static string GetHTML()
+        {
+            return        @"<html>
+                           <head></head>
+                           <body>
+                                   <table>
+                                    <tr>
+                                   <th>Ismi</th> 
+                                   <th>Familyasi</th>
+                                   <th>Jinsi</th>
+                                   <th>Kursi</th>
+                                   <th>yonalishi</th>
+                                   </tr>
+                                   <tr>
+                                   <th>Yahyo</th>
+                                   <th>Abdualimov</th>
+                                   <th>Erkak</th>
+                                   <th>3-kurs</th>
+                                   <th>Bank</th>
+                                   </tr>
+                                   <tr>
+                                   <th>Ahmadjon</th>
+                                   <th>Jo'rayev</th>
+                                   <th>Erkak</th>
+                                   <th>3-kurs</th>
+                                   <th>Backend</th>
+                                   </tr>
+                                   <tr>
+                                   <th>Najmiddin</th>
+                                   <th>Nazirov</th>
+                                   <th>Erkak</th>
+                                   <th>3-kurs</th>
+                                   <th>Frontend</th>
+                                   </tr>
+                                   </table>
+                           </body>
+                         </html>";
+        }
+    }
+}
